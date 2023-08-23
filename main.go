2653aea0d0e6879a8417d2a27e0592a62be5645d
Ángel Castañeda <angelaxel1420@disroot.org>
2023-08-23 00:15:55 -0500

    Make basic static http server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	http.ServeFile(w, r, filepath.Join("./", r.URL.Path))
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
-	log.Println("Hi!")
+	addr := flag.String("addr", ":4004", "HTTP Port Address")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", pageHandler)
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Println("Starting server on port ", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
